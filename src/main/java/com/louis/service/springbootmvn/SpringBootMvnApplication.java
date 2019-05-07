e958ee73b7fe2fcce585da98bbc8b2b2a19fdfce
wangxing <secret_dragon@163.com>
2019-05-07 16:17:15 +0800

    Schedule定时任务联系

--- src/main/java/com/louis/service/springbootmvn/SpringBootMvnApplication.java
+++ src/main/java/com/louis/service/springbootmvn/SpringBootMvnApplication.java
@@ -2,8 +2,10 @@ package com.louis.service.springbootmvn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootMvnApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/louis/service/springbootmvn/service/TestService.java
+++ src/main/java/com/louis/service/springbootmvn/service/TestService.java
@@ -1,6 +1,7 @@
 package com.louis.service.springbootmvn.service;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,9 +13,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService {
 
+    private int iCount1 = 1;
+    private int iCount2 = 1;
+
     @Async
-    public void generateReport(){
+    public void generateReport() {
         // 打印异步线程名称
-        System.out.printf("报表线程名："+Thread.currentThread().getName());
+        System.out.printf("报表线程名：" + Thread.currentThread().getName());
+    }
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    @Async
+    public void getScheduler1() {
+        System.out.printf("线程：" + Thread.currentThread().getName() + "。任务：1。执行次数：" + iCount1 + "\n");
+        iCount1++;
+    }
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    @Async
+    public void getScheduler2() {
+        System.out.printf("线程：" + Thread.currentThread().getName() + "。任务：2。执行次数：" + iCount2 + "\n");
+        iCount2++;
     }
 }
